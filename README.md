2e9a6a0cd2d66093b3dedbb31047fba13191d1d2
Simran <simran.dalv30@gmail.com>
2025-12-23 16:42:55 +0530

    Folder Structure Added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Medical_Chat_Bot
+
+project template
+using a bash script for project structure
+all stored in template.sh
+
+called this file on git terminall using 
+sh template.sh
---
+++ template.sh
@@ -0,0 +1,20 @@
+# creating a directory
+mkdir -p src
+mkdir -p research
+mkdir data
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+# touch Dockerfile
+
+
+
+
+echo "Directory and files created successfully"
